eb9c580db601c70711db8011253360b19e23ad9f
hasibhasan-github <hasibhasan483@gmail.com>
2024-08-02 23:42:44 +0600

    @hasibhasan-github

--- azureDeploy/deployment.py
+++ azureDeploy/deployment.py
@@ -27,12 +27,26 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 parameters = {pair.split('='):pair.split('=')[1] for pair in connection_string.split(' ')}
 
+# DATABASES = {
+#     'default' : {
+#         'ENGINE' : 'django.db.backends.postgresql',
+#         'NAME' : parameters['dbname'],
+#         'HOST' : parameters['host'],
+#         'USER' : parameters['user'],
+#         'PASSWORD' : parameters['password'],
+#     }
+# }
+
 DATABASES = {
-    'default' : {
-        'ENGINE' : 'django.db.backends.postgresql',
-        'NAME' : parameters['dbname'],
-        'HOST' : parameters['host'],
-        'USER' : parameters['user'],
-        'PASSWORD' : parameters['password'],
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'realtimechatazure-database',
+        'HOST': 'realtimechatazure-server.postgres.database.azure.com',
+        'PORT': '5432',
+        'USER': 'pqkphsxbke',
+        'PASSWORD': '6yAvQUgvJG2yT9j$',
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
--- azureDeploy/settings.py
+++ azureDeploy/settings.py
@@ -91,10 +91,24 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'realtimechatazure-database',
+        'HOST': 'realtimechatazure-server.postgres.database.azure.com',
+        'PORT': '5432',
+        'USER': 'pqkphsxbke',
+        'PASSWORD': '6yAvQUgvJG2yT9j$',
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
